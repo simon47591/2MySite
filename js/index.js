5d0f66e2a45bca5468f2beda452bb82da1e42a3c
Simon <simon47591@hotmail.com>
2015-07-15 15:03:13 -0400

    updates

--- js/index.js
+++ js/index.js
@@ -99,7 +99,7 @@ function onMobileReady(){
  });
 
 
-
+closePopup();
 // End onMobileReady()	
 }
 
@@ -245,7 +245,7 @@ function deleteVideos(){
         $("#imgPlaceholder").html('');
 		$("#textValVideo").html('').fadeOut(100);
 		$("#addFromGallery").fadeIn(100);
-	    $("#addFromCamera").fadeIn(100);
+	    //$("#addFromCamera").fadeIn(100);
     });
 }
 
@@ -389,3 +389,20 @@ function submitVideoDets(mediaFile){
             alert("An error has occurred: Code = " + error.code);
 			$("#form-submitPhoto-trap-wrap").html("An error has occurred: Code = "+error.code);
         }
+		
+		
+		
+		
+		
+		
+		
+		
+		
+// Close popup function
+function closePopup(){
+	$(".closePopup").click(function(){
+		var parEl = $(this).parents(".popup-wrap");
+		parEl.fadeOut(200);
+		$(".opac-BG").fadeOut(200);
+	});
+}
